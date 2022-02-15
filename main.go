148a15eaa6d71366327fd948fe1f8db7e2308640
k.khoruzhenko <k.khoruzhenko@finflowsoft.com>
2022-02-15 19:32:41 +0300

    add layouts, form list .html

--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"net/http"
 )
 
 type Rsvp struct {
 	Name, Email, Phone string
 	WillAttend         bool
+}
+
+type formData struct {
+	*Rsvp
+	Errors []string
 }
 
 var responses = make([]*Rsvp, 0, 10)
@@ -26,6 +32,50 @@ func loadTemplates() {
 	}
 }
 
+func welcomeHandler(writer http.ResponseWriter, request *http.Request) {
+	templates["welcome"].Execute(writer, nil)
+}
+
+func listHandler(writer http.ResponseWriter, request *http.Request) {
+	templates["list"].Execute(writer, responses)
+}
+
+func formHandler(writer http.ResponseWriter, request *http.Request) {
+	if request.Method == http.MethodGet {
+		templates["form"].Execute(writer, formData{
+			Rsvp:   &Rsvp{},
+			Errors: []string{},
+		})
+	} else if request.Method == http.MethodPost {
+		err := request.ParseForm()
+		if err == nil {
+			responseData := Rsvp{
+				Name:       request.Form["name"][0],
+				Email:      request.Form["email"][0],
+				Phone:      request.Form["phone"][0],
+				WillAttend: request.Form["willattend"][0] == "true",
+			}
+			responses = append(responses, &responseData)
+
+			if responseData.WillAttend {
+				templates["thanks"].Execute(writer, responseData.Name)
+			} else {
+				templates["sorry"].Execute(writer, responseData.Name)
+			}
+		}
+	}
+	// templates["form"].Execute(writer, nil)
+}
+
 func main() {
 	loadTemplates()
+
+	http.HandleFunc("/", welcomeHandler)
+	http.HandleFunc("/list", listHandler)
+	http.HandleFunc("/form", formHandler)
+
+	err := http.ListenAndServe(":5000", nil)
+	if err != nil {
+		panic(err)
+	}
 }
