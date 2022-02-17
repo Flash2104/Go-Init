d8d250f7b9d9b9910d4a94c66f825e866a2f9599
k.khoruzhenko <khoruzhenko.work@gmail.com>
2022-02-17 13:58:21 +0300

    go tools (cmd)

--- go_tools/main.go
+++ go_tools/main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
+	printHello()
+	for i := 0; i < 5; i++ {
+		i = i
+		printNumber(i)
+	}
+	fmt.Scanln()
+}
+
+func printHello() {
 	fmt.Println("Hello, Go")
+}
+
+func printNumber(number int) {
+	fmt.Println(number)
 }
