07b4e882812c150bea1cab224105b5b12dc4cd3b
k.khoruzhenko <khoruzhenko.work@gmail.com>
2022-02-23 23:05:23 +0300

    structs

--- 9_structs/main.go
+++ 9_structs/main.go
@@ -7,12 +7,20 @@ func main() {
 		name, category string
 		price          float64
 	}
-	kayak := Product{
-		name:     "Kayak",
-		category: "Watersports",
-		price:    275,
+	type StockLevel struct {
+		Product
+		count int
 	}
+	stockItem := StockLevel{
+		Product{"Kayak", "Watersports", 275.00},
+		100,
+	}
+	kayak := stockItem.Product
 	fmt.Println(kayak.name, kayak.category, kayak.price)
 	kayak.price = 300
 	fmt.Println("Changed price:", kayak.price)
+
+	fmt.Println("Name:", stockItem.Product.name)
+	fmt.Println("Price:", stockItem.Product.price)
+	fmt.Println("Count:", stockItem.count)
 }
