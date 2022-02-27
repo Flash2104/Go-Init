b346c3d9c131bea6bf8cb92f64b8506823a14a95
k.khoruzhenko <khoruzhenko.work@gmail.com>
2022-02-27 18:35:49 +0300

    packages

--- 11_packages/main.go
+++ 11_packages/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"packages/store"
+)
 
+func main() {
+	product := store.NewProduct("Kayak", "Watersports", 279)
+	fmt.Println("Name:", product.Name)
+	fmt.Println("Category:", product.Category)
+	fmt.Println("Price:", product.Price())
 }
--- 11_packages/store/product.go
+++ 11_packages/store/product.go
@@ -1,7 +1,19 @@
+// Package store provides types and methods
+// commonly required for online sales
 package store
 
 // Product describes an item for sale
 type Product struct {
-	Name, Category string
+	Name, Category string // Name and type of the product
 	price          float64
+}
+
+func NewProduct(name, category string, price float64) *Product {
+	return &Product{name, category, price}
+}
+func (p *Product) Price() float64 {
+	return p.price
+}
+func (p *Product) SetPrice(newPrice float64) {
+	p.price = newPrice
 }
