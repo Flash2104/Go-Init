4d5349c40ea031a7590c240c7f307b1f06290c1a
k.khoruzhenko <khoruzhenko.work@gmail.com>
2022-03-09 19:54:10 +0300

    channels

--- 13_concurrency/main.go
+++ 13_concurrency/main.go
@@ -10,8 +10,31 @@ func main() {
 	// a := <-timer.C
 	// fmt.Println("Start ticker:", a.Format(time.Nanosecond.String()))
 
-	CalcStoreTotal(Products)
-	// fmt.Println("End ticker:", (<-timer.C).Format(time.Nanosecond.String()))
-	// timer.Stop()
-	fmt.Println("main func complete")
+	// CalcStoreTotal(Products)
+	// // fmt.Println("End ticker:", (<-timer.C).Format(time.Nanosecond.String()))
+	// // timer.Stop()
+	// fmt.Println("main func complete")
+
+	fmt.Println("----------------------------")
+	fmt.Println("Sending and Receiving an Unknown Number of Values")
+
+	dispatchChannel := make(chan DispatchNotification, 100)
+	go DispatchOrders(dispatchChannel)
+	for {
+		if details, open := <-dispatchChannel; open {
+			fmt.Println("Dispatch To", details.Customer, ":", details.Quantity, "x", details.Name)
+		} else {
+			break
+		}
+	}
+	dispatchChannel = make(chan DispatchNotification, 100)
+
+	// or Enumerating Channel Values
+	fmt.Println("----------------------------")
+	fmt.Println("Enumerating Channel Values")
+	go DispatchOrders(dispatchChannel)
+
+	for details := range dispatchChannel {
+		fmt.Println("Dispatch To", details.Customer, ":", details.Quantity, "x", details.Name)
+	}
 }
